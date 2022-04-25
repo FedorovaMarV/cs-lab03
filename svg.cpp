a24917433ab3bff6f2612f3e4be33cced5954757
Marina <marfeka.com>
2022-04-25 13:16:09 +0300

    code: индивидуальное задание

--- svg.cpp
+++ svg.cpp
@@ -1,7 +1,11 @@
 #include "svg.h"
+#include <iostream>
+#include <vector>
+#include <math.h>
 
 void //функции вывода заголовка и окончания SVG
-svg_begin(double width, double height) {
+svg_begin(double width, double height)
+{
     cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
     cout << "<svg ";
     cout << "width='" << width << "' ";
@@ -10,21 +14,27 @@ svg_begin(double width, double height) {
     cout << "xmlns='http://www.w3.org/2000/svg'>\n";
 }
 
-void svg_text (double left, double baseline, string text)
+void
+svg_end()
 {
-   cout << "<text x='"<<left<<"' y='"<<baseline<<"'>"<<text<<"</text>";
-}
-
-void svg_rect (double x, double y, double width, double height, string stroke, string fill)
-{
-    cout << "<rect x='" << x << "' y='" << y <<"' width='"<< width <<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />";
-}
-
-void svg_end() {
     cout << "</svg>\n";
 }
 
-void show_histogram_svg(const vector<size_t>& bins) {
+void
+svg_text(double left, double baseline, string text)
+{
+    cout << "<text x='" << left << "' y='" << baseline << "' >" << text << "</text>";
+}
+
+void svg_rect(double x, double y, double width, double height, string stroke, string fill)
+{
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "' />";
+
+}
+
+void
+show_histogram_svg(const vector<size_t>& bins, const vector<size_t>& proc)
+{
     const auto IMAGE_WIDTH = 400;
     const auto IMAGE_HEIGHT = 300;
     const auto TEXT_LEFT = 20;
@@ -32,19 +42,37 @@ void show_histogram_svg(const vector<size_t>& bins) {
     const auto TEXT_WIDTH = 50;
     const auto BIN_HEIGHT = 30;
     const auto BLOCK_WIDTH = 10;
+    const auto PROCENT = 50;
+
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
-    svg_rect(TEXT_WIDTH, 0, bins[0] * BLOCK_WIDTH, BIN_HEIGHT);
-    svg_rect(0, 0, 100, 200);                    // svg_rect(0, 0, 100, 200, "black", "black");
-    svg_rect(0, 0, 100, 200, "red");             // svg_rect(0, 0, 100, 200, "red",   "black");
-    svg_rect(0, 0, 100, 200, "blue", "#aaffaa");
-    double top = 0;
+
+    const auto MAX_BIN_WIDTH = IMAGE_WIDTH - TEXT_WIDTH - PROCENT;
+
+    size_t max_bins_width = bins[0];
     for (size_t bin : bins)
     {
-        const double bin_width = BLOCK_WIDTH * bin;
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+        if (bin > max_bins_width)
+        {
+            max_bins_width = bin;
+        }
+    }
+
+    max_bins_width = max_bins_width * BLOCK_WIDTH;
+
+    double top = 0;
+    for (size_t i = 0; i < bins.size(); i++)
+    {
+        double bin_width = BLOCK_WIDTH * bins[i];
+        if (max_bins_width >= MAX_BIN_WIDTH)
+        {
+            bin_width = MAX_BIN_WIDTH * (bin_width / max_bins_width) - 1;
+        }
+
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bins[i]));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "black", "blue");
+        svg_text (MAX_BIN_WIDTH, top + TEXT_BASELINE, to_string(proc[i]) + '%');
         top += BIN_HEIGHT;
+
     }
     svg_end();
 }
