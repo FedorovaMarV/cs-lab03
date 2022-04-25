d589ceb6fe3e58fc1cb8a168ab62dca13eb0ae69
Marina <marfeka.com>
2022-04-25 13:15:31 +0300

    code: индивидуальное задание

--- svg.h
+++ svg.h
@@ -1,21 +1,17 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 // SVG
-void
-svg_begin(double width, double height);
+void svg_begin(double width, double height);
 
-void
-svg_end();
+void svg_text (double left, double baseline, string text);
 
-void
-svg_text(double left, double baseline , string text);
+void svg_rect (double x, double y, double width, double height, string stroke = "black", string fill = "black");
 
-void
-svg_rect (double x, double y, double width, double height, string stroke = "black", string fill = "black");
+void svg_end();
 
-void
-show_histogram_svg(const vector<size_t>& bins);
+void show_histogram_svg(const vector<size_t>& bins, const vector<size_t>& proc);
