da02f0b4c7be60607e2e2e7c8f532383689af819
Marina <marfeka.com>
2022-04-25 13:16:39 +0300

    code: индивидуальное задание

--- histogram.cpp
+++ histogram.cpp
@@ -3,21 +3,32 @@
 void find_minmax(const vector<double>& numbers, double& min, double& max)
 {
     if (numbers.size() == 0)
-    {
         return;
-    }
+
     else
-    min = numbers[0];
-    max = numbers [0];
-    for (double number : numbers)
+    {
+        min = numbers[0];
+        max = numbers[0];
+        for (double number : numbers)
         {
-        if (number < min)
+            if (number < min)
+            {
+                min = number;
+            }
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+    }
+}
+
+vector <size_t> procent (size_t number_count, const vector <size_t> bins)
+{
+        vector <size_t> x (bins.size());
+        for ( size_t i = 0; i < bins.size(); i++ )
         {
-            min = number;
+            x[i] = static_cast<double> (bins[i]) / number_count * 100;
         }
-        if (number > max)
-         {
-            max = number;
-         }
-        }
+        return x;
 }
