4cb8d16b8b309e423ad799181bd3eafb644c5bfb
Marina <marfeka.com>
2022-05-27 13:27:28 +0300

    загрузка файла по сети

--- main.cpp
+++ main.cpp
@@ -43,68 +43,28 @@ Input read_input(istream& in, bool prompt)
 }
 
 
-void show_histogramm_text(const vector<size_t>& bins)
-{
-    size_t SCREEN_WIDTH = 80;
-    const size_t max_asterix = SCREEN_WIDTH - 3 - 1;
-
-    size_t max_bin = bins[0];
-    for (size_t bin: bins)
-    {
-        if (max_bin < bin)
-        {
-            max_bin = bin;
-        }
-    }
-
-    for(size_t bin: bins)
-    {
-        size_t height = bin;
-
-//Проверка, нужно ли масштабировать столбцы
-        if (max_bin > max_asterix)
-        {
-            height = max_asterix * (static_cast<double>(bin) / max_bin);
-        }
-
-        if (bin < 100)
-        {
-            cout << ' ';
-        }
-        if (bin < 10)
-        {
-            cout << ' ';
-        }
-        cout << bin << "|";
-        for (size_t i = 0; i < height; i++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
-}
-
 int main(int argc, char* argv[])
 {
     if(argc > 1)
     {
-        cout << "argc = " << argc << '\n';
-        for(size_t i = 0; i < argc; i++)
+        curl_global_init(CURL_GLOBAL_ALL);
+        CURL *curl = curl_easy_init();
+        if(curl)
         {
-            cout << "argv[" << i << "]= " << argv[i] << '\n';
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
         }
         return 0;
     }
 
 
-    curl_global_init(CURL_GLOBAL_ALL);
-//Ввод данных
-    Input data;
+
     const auto input = read_input(cin, true);
-
-//Расчет гистограммы
     const auto bins = make_histogramm(input);
-
-//Вывод гистограммы
     show_histogramm_svg(bins);
+
+    return 0;
 }
+
