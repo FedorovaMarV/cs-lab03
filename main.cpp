955d045f6493f3929b651ef88ab04cb7d2a4ca01
Marina <marfeka.com>
2022-05-28 19:07:56 +0300

    сохранение данных из сети в буфер

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "histogram.h"
 #include "svg.h"
 #include <curl/curl.h>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -42,35 +44,55 @@ Input read_input(istream& in, bool prompt)
     return data;
 }
 
-
-int main(int argc, char* argv[])
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx)
 {
-    if(argc > 1)
-    {
-        curl_global_init(CURL_GLOBAL_ALL);
-        CURL *curl = curl_easy_init();
-        if(curl)
-        {
-            CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-            res = curl_easy_perform(curl);
-            curl_easy_cleanup(curl);
-            if (res)
-            {
-                cerr << curl_easy_strerror(res) << endl;
-                exit(1);
-            }
-        }
-        return 0;
-    }
-
-
-
-    const auto input = read_input(cin, true);
-    const auto bins = make_histogramm(input);
-
-    show_histogramm_svg(bins);
-
-    return 0;
+// TODO: дописывать данные к буферу.
+    size_t data_size=item_size*item_count;
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(reinterpret_cast<const char*>(items), data_size);
+    return data_size;
 }
 
+Input
+download(const string& address)
+{
+    stringstream buffer;
+
+// TODO: заполнить буфер.
+    curl_global_init(CURL_GLOBAL_ALL);
+    CURL *curl = curl_easy_init();
+    if(curl)
+    {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        res = curl_easy_perform(curl);
+        if (res)
+        {
+            cerr << curl_easy_strerror(res) << endl;
+            exit(1);
+        }
+    }
+    curl_easy_cleanup(curl);
+    return read_input(buffer, false);
+
+}
+
+int
+main(int argc, char* argv[])
+{
+    Input input;
+    if (argc > 1)
+    {
+        input = download(argv[1]);
+    }
+    else
+    {
+        input = read_input(cin, true);
+    }
+
+    const auto bins = make_histogramm(input);
+    show_histogramm_svg(bins);
+}
