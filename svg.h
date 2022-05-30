4d371b118bcca72e059d118e2d160141814cf968
Marina <marfeka.com>
2022-05-30 14:39:54 +0300

    окончательный коммит

--- svg.h
+++ svg.h
@@ -1,15 +1,15 @@
 #ifndef SVG_H_INCLUDED
 #define SVG_H_INCLUDED
-#include <iostream>
 #include <vector>
-#include <string>
-
+#include <iostream>
+#include <sstream>
+#include <windows.h>
 using namespace std;
 
 void svg_begin(double width, double height);
-void svg_text(double left, double baseline, string text);
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black");
 void svg_end();
-void show_histogramm_svg(const vector<size_t>& bins);
-
+void svg_text(double left, double baseline, string text);
+void svg_rect(double x, double y, double width, double height, string stroke, string fill);
+string make_info_text();
+void show_histogram_svg(const vector<size_t>& bins);
 #endif // SVG_H_INCLUDED
