698f342d523f593d48947496419b6100dacbf127
Marina <marfeka.com>
2022-05-30 12:59:26 +0300

    форматный вывод в стиле С

--- main.cpp
+++ main.cpp
@@ -5,81 +5,81 @@
 
 vector<double> input_numbers(size_t count)
 {
-vector<double> result(count);
-for(size_t i = 0; i < count; i++)
-{
-cin >> result[i];
-}
-return result;
+    vector<double> result(count);
+    for(size_t i = 0; i < count; i++)
+    {
+        cin >> result[i];
+    }
+    return result;
 }
 
 vector<size_t> make_histogramm(const vector<double>& numbers, size_t bin_count)
 {
-double min, max;
-find_minmax(numbers, min, max);
+    double min, max;
+    find_minmax(numbers, min, max);
 
-vector<size_t> bins(bin_count, 0);
-double bin_size = (max - min) / bin_count;
-for (size_t i = 0; i < numbers.size(); i++)
-{
-bool found = false;
-for(size_t j = 0; j < (bin_count - 1) && !found; j++)
-{
-auto lo = min + j * bin_size;
-auto hi = min + (j + 1) * bin_size;
-if (lo <= numbers[i] && numbers[i] < hi)
-{
-bins[j]++;
-found = true;
-}
-}
-if (!found)
-{
-bins[bin_count - 1]++;
-}
-}
-return bins;
+    vector<size_t> bins(bin_count, 0);
+    double bin_size = (max - min) / bin_count;
+    for (size_t i = 0; i < numbers.size(); i++)
+    {
+        bool found = false;
+        for(size_t j = 0; j < (bin_count - 1) && !found; j++)
+        {
+            auto lo = min + j * bin_size;
+            auto hi = min + (j + 1) * bin_size;
+            if (lo <= numbers[i] && numbers[i] < hi)
+            {
+                bins[j]++;
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            bins[bin_count - 1]++;
+        }
+    }
+    return bins;
 }
 
 void show_histogramm_text(const vector<size_t>& bins)
 {
-size_t SCREEN_WIDTH = 80;
-const size_t max_asterix = SCREEN_WIDTH - 3 - 1;
+    size_t SCREEN_WIDTH = 80;
+    const size_t max_asterix = SCREEN_WIDTH - 3 - 1;
 
-size_t max_bin = bins[0];
-for (size_t bin: bins)
-{
-if (max_bin < bin)
-{
-max_bin = bin;
-}
-}
+    size_t max_bin = bins[0];
+    for (size_t bin: bins)
+    {
+        if (max_bin < bin)
+        {
+            max_bin = bin;
+        }
+    }
 
-for(size_t bin: bins)
-{
-size_t height = bin;
+    for(size_t bin: bins)
+    {
+        size_t height = bin;
 
 //Проверка, нужно ли масштабировать столбцы
-if (max_bin > max_asterix)
-{
-height = max_asterix * (static_cast<double>(bin) / max_bin);
-}
+        if (max_bin > max_asterix)
+        {
+            height = max_asterix * (static_cast<double>(bin) / max_bin);
+        }
 
-if (bin < 100)
-{
-cout << ' ';
-}
-if (bin < 10)
-{
-cout << ' ';
-}
-cout << bin << "|";
-for (size_t i = 0; i < height; i++)
-{
-cout << "*";
-}
-cout << endl;
-}
+        if (bin < 100)
+        {
+            cout << ' ';
+        }
+        if (bin < 10)
+        {
+            cout << ' ';
+        }
+        cout << bin << "|";
+        for (size_t i = 0; i < height; i++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
 
 
@@ -87,29 +87,35 @@ cout << endl;
 int main()
 {
 //Ввод данных
-size_t number_count;
-cerr << "Enter number count: ";
-cin >> number_count;
-if(number_count == 0)
-{
-cerr << "There is nothing to compute\n";
-return 0;
-}
+    const char* name = "Commander Shepard";
+    int year = 2154;
+    printf("%s was born in %d.\n", name, year);
+    printf("n = %08x\n", 0x1234567); // 01234567
+    return 0;
+
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+    if(number_count == 0)
+    {
+        cerr << "There is nothing to compute\n";
+        return 0;
+    }
 
 
-cerr << "Enter numbers: ";
-const auto numbers = input_numbers(number_count);
+    cerr << "Enter numbers: ";
+    const auto numbers = input_numbers(number_count);
 
-size_t bin_count;
-cerr << "Enter bin count: ";
-cin >> bin_count;
+    size_t bin_count;
+    cerr << "Enter bin count: ";
+    cin >> bin_count;
 
 
 //Расчет гистограммы
-const auto bins = make_histogramm(numbers, bin_count);
+    const auto bins = make_histogramm(numbers, bin_count);
 
 //Вывод гистограммы
-show_histogramm_svg(bins);
+    show_histogramm_svg(bins);
 
-return 0;
+    return 0;
 }
