2c5cf0f2a4d35829ed1c7dde82e9a2c31660cfb5
Marina <marfeka.com>
2022-05-30 12:28:37 +0300

    4.2 индивидуальное задание

--- main.cpp
+++ main.cpp
@@ -8,58 +8,64 @@
 
 using namespace std;
 
-vector<double> input_numbers(istream& in, size_t count)
-{
+vector<double>
+input_numbers(istream& in, size_t count) {
     vector<double> result(count);
-    for(size_t i = 0; i < count; i++)
-    {
+    for (size_t i = 0; i < count; i++) {
         in >> result[i];
     }
     return result;
 }
 
-Input read_input(istream& in, bool prompt)
-{
+Input
+read_input(istream& in, bool promt) {
     Input data;
 
+    if(promt)
+    cerr << "Enter number count: ";
+
     size_t number_count;
-    if (prompt)
-    {
-        cerr << "Enter number count: ";
-    }
     in >> number_count;
 
-    if(prompt)
-    {
-        cerr << "Enter numbers: ";
-    }
+    if(promt)
+    cerr << "Enter numbers: ";
+
     data.numbers = input_numbers(in, number_count);
 
-    if(prompt)
-    {
-        cerr << "Enter bin count: ";
-    }
+    if(promt)
+    cerr << "Enter column count: ";
+
+
     in >> data.bin_count;
 
     return data;
 }
 
 size_t
-write_data(void* items, size_t item_size, size_t item_count, void* ctx)
-{
-// TODO: дописывать данные к буферу.
-    size_t data_size=item_size*item_count;
+write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+
+    size_t data_size = item_size * item_count;
+
     stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
     buffer->write(reinterpret_cast<const char*>(items), data_size);
+
     return data_size;
 }
 
-Input
-download(const string& address)
-{
-    stringstream buffer;
+int progress_callback(double dltotal,   double dlnow){
+    double result;
+    result=100*dlnow/dltotal;
+    if (dltotal=0) {
+        cerr << "0%" << endl;
+    }
+    cerr << "progress: " << result << "%" << endl;
+    return CURL_PROGRESSFUNC_CONTINUE;
+}
 
-// TODO: заполнить буфер.
+Input
+download(const string& address) {
+
+    stringstream buffer;
     curl_global_init(CURL_GLOBAL_ALL);
     CURL *curl = curl_easy_init();
     if(curl)
@@ -68,31 +74,39 @@ download(const string& address)
         curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, progress_callback);
+        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
         res = curl_easy_perform(curl);
-        if (res)
+        curl_easy_cleanup(curl);
+
+        if(res)
         {
-            cerr << curl_easy_strerror(res) << endl;
+            cout <<  curl_easy_strerror(res);
             exit(1);
         }
-    }
-    curl_easy_cleanup(curl);
-    return read_input(buffer, false);
 
+
+    }
+
+    return read_input(buffer, false);
 }
 
-int
-main(int argc, char* argv[])
+
+int main(int argc, char* argv[])
 {
     Input input;
+    cerr << argc;
     if (argc > 1)
     {
         input = download(argv[1]);
     }
+
     else
     {
-        input = read_input(cin, true);
+         input = read_input(cin, true);
     }
+    const auto bins = make_histogram(input);
+    show_histogram_svg(bins);
 
-    const auto bins = make_histogramm(input);
-    show_histogramm_svg(bins);
+    return 0;
 }
